c165fd04415d8448b8119f7d72ef5601b2c9035c
Bishu Das <bishudash@gmail.com>
2014-09-02 17:17:50 -0700

    refined. temp reading in C at max possible resolution now.

--- py/GridEye_RGBonly_v1.py
+++ py/GridEye_RGBonly_v1.py
@@ -12,9 +12,11 @@ Dependencies:
 """
 
 #headers and imports.
+from __future__ import print_function
 from serial import *
 import atexit
 from threading import Thread
+import sys
 from struct import unpack
 import Image
 
@@ -22,6 +24,7 @@ import Image
 #load 8px,8px image with white background.
 img = Image.new( 'RGB', (8,8), "white") 
 pixels = img.load()
+fl = open('data.dat', 'a')
 
 #global variables to keep track of event frame.
 count=0
@@ -36,50 +39,56 @@ output : no returns. saves image from PIR to gg#.jpg for all # in int.
 """
 def receiving(ser):
     global count
-    while True:
-
-        #read the first 5 bytes. {42,42,42,ThermistorL,ThermistorH}
-        ser.read(5)	
-        
-        #increment out frame count. we are ready to stream.
-        count+=1
-        
-        #range=8*8=64. for each we separate low bytes, print to image.
-        for i in range(64):
-            try:
-                buf1 = ser.read()
-                vg= unpack('B', buf1)
-                #this will read high byte. to be ignored.
-                buf1 = ser.read()
-                
-                #vg[0] has the temperature value in F. 
-                if(int(vg[0])<80):
-                    color=(0,0,vg[0])
-                elif(int(vg[0])>80 and int(vg[0])<100):
-                    color=((vg[0]-80)*50/20,(100-vg[0])*255/20,0)
-                else:
-                    color=((100-vg[0])*255/20,(vg[0]-100)*50/20,0)
-                pixels[7-(i%8),7-(i/8)]=color
-            except:
-                print()
-        
-        #pprint for images basically. resizing antialias to 500x500
-        #from 8x8
-        newim = img.resize((500,500), Image.ANTIALIAS)
-        newim.save('gg'+str(count)+'.jpg')
-        
-"""
-input parameters: ser -> serial device in usage.
-output : N/A
-
-This function executes when we exit/terminate our python script.
-This will make sure that out serial device (PIC24F04KA200) is not 
-continuing to run after we terminate the appication. Write a '~'!!
-"""
+    while(True):
+		try:
+			ser.read()
+			ser.read()
+			ser.read()
+			lsb=ser.read()
+			msb=ser.read()
+			therm=int((ord(msb)<<8)+ord(lsb))
+			celTherm=therm*0.0625
+			count+=1
+			print("AVG TEMP: "+str(celTherm))
+			for i in range(64):
+				try:
+					lsb=ser.read()
+					msb=ser.read()
+					temp=int((ord(msb)<<8)+ord(lsb))
+					while (temp>2047):
+						temp = 2048-temp
+					celcius=0.25*temp
+					if (celcius>0):
+						fl.write(str(celcius))
+						fl.write(",")
+						print(celcius)
+					else:
+						return
+					if(int(celcius)>int(celTherm)):
+						pixels[7-(i%8),7-(i/8)]=(200,0,0)
+					elif(int(celcius)<int(celTherm)):
+						pixels[7-(i%8),7-(i/8)]=(0,0,200)
+					else:
+						pixels[7-(i%8),7-(i/8)]=(0,100,0)
+				except Exception, e:
+					e.args += (color,temp,)
+					raise
+			newim = img.resize((500,500), Image.ANTIALIAS)
+			newim.save('gg'+str(count)+'.jpg')
+			fl.write("\n\n\n")
+		except Exception, e:
+			ser.open()
+			ser.write('~')
+			ser.write('~')
+			ser.write('*')
+			pass
+    pass
+		
 def exitSafe(ser):
 	try:
 		ser.write('~')
 		ser.close();
+		fl.close()
 	except:
 		pass
 
@@ -117,6 +126,5 @@ if __name__ ==  '__main__':
 
 	#dynamic thread to process received data.
     Thread(target=receiving, args=(ser,)).start()
+    atexit.register(exitSafe, ser)
     
-    #function to make sure we have a termination procedure in place.
-	atexit.register(exitSafe, ser)
